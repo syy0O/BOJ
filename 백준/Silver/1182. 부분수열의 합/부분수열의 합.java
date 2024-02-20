37d33bc66db818215851bd3efa7cbf757f6f711f
syy0O <86238720+syy0O@users.noreply.github.com>
2024-02-20 15:36:39 +0900

    [Silver II] Title: 부분수열의 합, Time: 136 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,59 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+    private static int ans = 0;
+    private static int N, S;
+    private static int[] numbers;
+    private static boolean[] isUsed;
 
-    public static int result;
-    public static int[] numbers;
-    public static boolean[] used;
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int S = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
         numbers = new int[N];
+        isUsed = new boolean[N];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             numbers[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 1; i <= N; i++) {
-            used = new boolean[N];
-            for (int j = 0; j < numbers.length; j ++) {
-                used[j] = true;
+        findSubtotal(0, 0);
 
-            }
-        }
+        System.out.println(ans);
 
-        for (int i = 0; i < numbers.length; i++) {
-            used = new boolean[N];
-            used[i] = true;
-            for (int m = 1; m <= N - i; m++) { // maxNumCnt
-                backtrack(m, 1, numbers[i], S, i);
-            }
-        }
-
-        System.out.println(result);
     }
 
-    public static void backtrack(int numMaxCnt, int numCnt, int sum, int S, int beginIdx) {
-        if (numMaxCnt == numCnt) {
-            if (S == sum) {
-                result++;
-            }
+
+    public static void findSubtotal(int sum, int idx){
+        if (sum == S && idx != 0) {
+            ans++;
+        }
+
+        if (idx == N) {
             return;
         }
 
-        for (int i = beginIdx; i < numbers.length; i++) {
-            if (!used[i]) {
-                used[i] = true;
-                backtrack(numMaxCnt, numCnt + 1, sum + numbers[i], S, i);
-                used[i] = false;
+        for (int i = idx; i < N; i++) {
+            if (!isUsed[i]) {
+                isUsed[i] = true;
+                findSubtotal(sum + numbers[i], i + 1); 
+                isUsed[i] = false;
             }
         }
     }

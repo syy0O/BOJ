eef4626608f2240321131a19b571888780516705
유송연 <86238720+syy0O@users.noreply.github.com>
2024-04-04 16:45:04 +0900

    [Gold III] Title: Watering the Fields, Time: 1580 ms, Memory: 92656 KB -BaekjoonHub

--- 백준/Gold/10021. Watering the Fields/Watering the Fields.java
+++ 백준/Gold/10021. Watering the Fields/Watering the Fields.java
@@ -1,119 +1,106 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.awt.Point;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+    private static class Edge implements Comparable<Edge> {
+        int start, end;
+        int cost;
 
-    private static int N;
-    private static int C;
-    private static int[] x;
-    private static int[] y;
-    private static int[] parent;
+        public Edge(int start, int end, int cost) {
+            this.start = start;
+            this.end = end;
+            this.cost = cost;
+        }
 
+        @Override
+        public int compareTo(Edge e) {
+            return cost - e.cost;
+        }
+    }
+
+    private static Point[] points;
+    private static int[] parents;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
 
-        N = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+        parents = new int[N];
+        points = new Point[N];
 
-        x = new int[N];
-        y = new int[N];
-        parent = new int[N + 1];
-
-        for (int i = 1; i <= N; i++) {
-            parent[i] = i;
+        for (int i = 0; i < N; i++) {
+            parents[i] = i;
         }
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-
-            x[i] = Integer.parseInt(st.nextToken());
-            y[i] = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            points[i] = new Point(x, y);
         }
 
-        PriorityQueue<Node> queue = new PriorityQueue<>((o1, o2) -> (int) (o1.value - o2.value));
-
-        for (int i = 0; i < N - 1; i++) {
+        Queue<Edge> pq = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
             for (int j = i + 1; j < N; j++) {
-                long dist = (x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]);
-
-                queue.offer(new Node(i, j, dist));
+                int cost = (int)(Math.pow(points[i].x - points[j].x, 2) + Math.pow(points[i].y - points[j].y, 2));
+                pq.add(new Edge(i, j, cost));
             }
         }
 
+        int cnt = 0;
         int sum = 0;
-        int count = 0;
 
-        while (!queue.isEmpty()) {
-            Node node = queue.poll();
-
-            if (node.value < C) {
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
+            if (edge.cost < C) {
                 continue;
             }
 
-            int x = node.start;
-            int y = node.end;
+            int start = edge.start;
+            int end = edge.end;
 
-            if (!isSameParent(x, y)) {
-                sum += node.value;
-                union(x, y);
-                count++;
+            if (!isSameParent(start, end)) {
+                sum += edge.cost;
+                union(start, end);
+                cnt++;
             }
 
-            if (count == N - 1) {
+            if (cnt == N - 1) {
                 break;
             }
         }
 
-        if (count != N - 1) {
-            System.out.println(-1);
-
-            return;
+        if (cnt != N - 1) {
+           sum = -1;
         }
 
         System.out.println(sum);
     }
 
-    private static int find(int x) {
-        if (x == parent[x]) {
-            return x;
-        }
+    public static boolean isSameParent(int start, int end) {
+        int startParent = find(start);
+        int endParent = find(end);
 
-        return find(parent[x]);
+        return (startParent == endParent);
     }
 
-    private static void union(int x, int y) {
-        x = find(x);
-        y = find(y);
+    public static int find(int idx) {
+        if (idx == parents[idx]) {
+            return idx;
+        }
+        return parents[idx] = find(parents[idx]);
+    }
 
-        if (x != y) {
-            parent[y] = x;
+    public static void union(int start, int end) {
+        start = find(start);
+        end = find(end);
+
+        if (start != end) {
+            parents[end] = start;
         }
     }
 
-    private static boolean isSameParent(int x, int y) {
-        x = find(x);
-        y = find(y);
-
-        if (x == y) {
-            return true;
-        }
-
-        return false;
-    }
-
-    static class Node {
-        private int start;
-        private int end;
-        private long value;
-
-        public Node(int start, int end, long value) {
-            this.start = start;
-            this.end = end;
-            this.value = value;
-        }
-    }
 }
+

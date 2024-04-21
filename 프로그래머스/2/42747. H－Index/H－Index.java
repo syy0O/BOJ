873338105e30c155e9285a0edb8814e91ee045eb
유송연 <86238720+syy0O@users.noreply.github.com>
2024-04-21 18:45:52 +0900

    [level 2] Title: H-Index, Time: 0.33 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        
+        for (int i=citations.length-1; i>=0;i--){
+            if (citations[i] <= citations.length-i) {break;}
+            
+            answer++;
+        }
+        
+        return answer;
+    }
+}

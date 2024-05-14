d10efbfb6a7a4e74071be34c35e438324f2219a9
유송연 <86238720+syy0O@users.noreply.github.com>
2024-05-14 19:28:30 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.10 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,12 +1,12 @@
 class Solution {
     public long solution(int n) {
-        long answer = 0;
-        int[] dp = new int[2001];  
+        long answer;
+        int[] dp = new int[2001];
         dp[1] = 1;
         dp[2] = 2;
         
-        for (int i=3; i<=n;i++) {
-            dp[i] = (dp[i-1] + dp[i-2]) % 1_234_567;
+        for (int i=3;i<=n;i++) {
+            dp[i] = (dp[i-2] + dp[i-1]) % 1_234_567;
         }
         
         answer = dp[n];

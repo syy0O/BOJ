26dc38619f680d10bf355f78609cedc385a51b8c
유송연 <86238720+syy0O@users.noreply.github.com>
2024-05-24 14:19:53 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.11 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,46 +2,35 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 class Solution {
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
-       Deque<Integer> queue = new ArrayDeque<>();
+    public int solution(int bridge_length, int weight, int[] truck_weights){
+         Deque<Integer> queue = new ArrayDeque<>();
 
-        int answer = 0;
-        int time = 0;
-        int sum = 0;
+        int answer = 0; // 경과된 시간
+        int cnt = 0; // 건넌 트럭의 개수
+        int total = truck_weights.length;
 
-        for (int i = 0; i < truck_weights.length; i++) {
+        int idx = 0;
+        int currWeightSum = 0;
+        while (cnt < total) {
+            answer++;
 
-            int truck = truck_weights[i];
-
-            while (true) {
-                if (queue.isEmpty()) {
-                    queue.addLast(truck);
-                    time++;
-                    sum += truck;
-                    break;
-                }
-
-                else if (queue.size() == bridge_length) {
-                    sum -= queue.pollFirst();
-                }
-
-                else {
-                    if (sum + truck <= weight) {
-                        queue.addLast(truck);
-                        time++;
-                        sum += truck;
-                        break;
-                    }
-                    else {
-                        queue.addLast(0);
-                        time++;
-                    }
+            if (queue.size() == bridge_length) {
+                int w = queue.pollFirst();
+                currWeightSum -= w;
+                if (w != 0) {
+                    cnt++;
                 }
             }
 
-        }
+            if (idx < total && currWeightSum + truck_weights[idx] <= weight) {
+                currWeightSum += truck_weights[idx];
+                queue.addLast(truck_weights[idx]);
+                idx++;
+                continue;
+            }
 
-        answer  = time + bridge_length;
+            queue.addLast(0);
+        }
 
 
         return answer;

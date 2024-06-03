3861c88e08837bb70464777559f2cf9d557a8b2a
유송연 <86238720+syy0O@users.noreply.github.com>
2024-06-03 20:09:42 +0900

    [level 1] Title: 과일 장수, Time: 0.48 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        Arrays.sort(score);
+        
+        for (int i=score.length-1;i>=0;i-=m) {
+            
+            if (i-m+1 >= 0) {
+                answer += m * score[i-m+1];
+            }
+        }
+        
+     
+        
+        
+        return answer;
+    }
+}

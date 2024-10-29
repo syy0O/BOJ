a7f506c6718d031db698d583e8449c7fd772bbcc
유송연 <86238720+syy0O@users.noreply.github.com>
2024-10-29 09:59:49 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.63 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,26 +1,37 @@
-import java.util.HashMap;
+// 1명이상 - 10만명 이하
+// O(NlogN)
+// 두번 반복문 도는 순간 끝남.
+
+// 어차피 participant 배열 만큼은 돌아야함 (100000번)
+import java.util.*;
+
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-       String answer = "";
-        HashMap<String, Integer> countOfName = new HashMap<>();
-        for (String s : completion) {
-            countOfName.put(s, countOfName.getOrDefault(s, 0) + 1);
-        }
-
+        String answer = "";
         
-        for (String s : participant) {
-            if (!countOfName.containsKey(s)) {
-                answer = s;
+        // 동명이인이 있을 수 있음 -> 키로만 구분하면 안되고 값으로 구분해야함.
+        HashMap<String, Integer> completions = new HashMap<>();
+        for (int i=0;i<completion.length;i++) {
+            String key = completion[i];
+            completions.put(key, completions.getOrDefault(key, 0) + 1);
+        }
+        
+      
+        // participant가 키에 있는지 확인. 없으면 바로 나가리.
+        for (int i=0;i<participant.length;i++) {
+            String key = participant[i];
+            
+            // 아예 키가 존재하지 않거나, 키의 값이 0일 때
+            if (!completions.containsKey(key) || completions.get(key) == 0) {
+                answer = key;
                 break;
             }
-
-            countOfName.put(s, countOfName.get(s) - 1);
             
-            if (countOfName.get(s) == 0) {
-                countOfName.remove(s);
-            }
+            completions.put(key, completions.get(key) - 1);
         }
+       
+        
         
         return answer;
     }

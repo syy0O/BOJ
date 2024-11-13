82e29d096a47baa199dc6055f47405f001dc76ba
유송연 <86238720+syy0O@users.noreply.github.com>
2024-11-14 08:11:58 +0900

    [level 3] Title: 아방가르드 타일링, Time: 63.62 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/3/181186. 아방가르드 타일링/아방가르드 타일링.java
+++ 프로그래머스/3/181186. 아방가르드 타일링/아방가르드 타일링.java
@@ -1,27 +1,32 @@
-import java.math.BigInteger;
-
 class Solution {
     private final int MAX = 100_000;
-    private final BigInteger MOD = BigInteger.valueOf(1_000_000_007);
-
     public int solution(int n) {
-        BigInteger[] dp = new BigInteger[MAX + 1];
-        dp[0] = BigInteger.ONE;
-        dp[1] = BigInteger.ONE;
-        dp[2] = BigInteger.valueOf(3);
-        dp[3] = BigInteger.valueOf(10);
-        dp[4] = BigInteger.valueOf(23);
-        dp[5] = BigInteger.valueOf(62);
+        
+        int[] dp = new int[MAX+1];
+        dp[1] = 1;
+        dp[2] = 3;
+        dp[3] = 10;
+        
 
-        for (int i = 6; i <= n; i++) {
-            dp[i] = dp[i - 1]
-                    .add(dp[i - 2].multiply(BigInteger.valueOf(2)))
-                    .add(dp[i - 3].multiply(BigInteger.valueOf(6)))
-                    .add(dp[i - 4])
-                    .subtract(dp[i - 6])
-                    .mod(MOD);
+        for (int i=4;i<=n;i++) {
+            
+            dp[i] = dp[i-1] * 2 -1; 
+            
+            if (i == 4) {
+                dp[i] += 4;    
+            }
+            
+            if (i == 5) {
+                dp[i] += 8;
+            }
+            
+            if (i == 6) {
+                dp[i] += 17;
+            }
         }
-
-        return dp[n].mod(MOD).intValue();
+        
+        int answer = dp[n];
+        
+        return answer;
     }
 }

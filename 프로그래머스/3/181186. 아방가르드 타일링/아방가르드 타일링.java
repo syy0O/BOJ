3e5f4f9f20c5a42211ae0697b203b54e2519740b
유송연 <86238720+syy0O@users.noreply.github.com>
2024-11-14 08:59:00 +0900

    [level 3] Title: 아방가르드 타일링, Time: 8.76 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/3/181186. 아방가르드 타일링/아방가르드 타일링.java
+++ 프로그래머스/3/181186. 아방가르드 타일링/아방가르드 타일링.java
@@ -1,32 +1,24 @@
+// dp[x] - dp[x-3] = dp[x-1] + dp[x-2] * 2 + dp[x-3] * 5 + dp[x-4] - dp[x-6]
+// dp[x] = dp[x-1] + dp[x-2] * 2 + dp[x-3] * 6 + dp[x-4] - dp[x-6]
+
 class Solution {
     private final int MAX = 100_000;
+    private final int MOD = 1_000_000_007;
+    
     public int solution(int n) {
         
-        int[] dp = new int[MAX+1];
-        dp[1] = 1;
+        long[] dp = new long[MAX+1];
+        dp[0] = 1;
+        dp[1] = 1; 
         dp[2] = 3;
         dp[3] = 10;
+        dp[4] = 23;
+        dp[5] = 62;
         
-
-        for (int i=4;i<=n;i++) {
-            
-            dp[i] = dp[i-1] * 2 -1; 
-            
-            if (i == 4) {
-                dp[i] += 4;    
-            }
-            
-            if (i == 5) {
-                dp[i] += 8;
-            }
-            
-            if (i == 6) {
-                dp[i] += 17;
-            }
+        for(int i = 6; i <= n; i++){
+           dp[i] = (dp[i-1] + (dp[i-2] * 2) % MOD + (dp[i-3] * 6) % MOD + dp[i-4] % MOD - dp[i-6] % MOD + MOD) % MOD;
         }
         
-        int answer = dp[n];
-        
-        return answer;
+        return (int)(dp[n] % MOD);
     }
 }

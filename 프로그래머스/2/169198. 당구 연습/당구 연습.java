2e8b5c12bc7fbfb9afafee0bf51a595fe86e629f
유송연 <86238720+syy0O@users.noreply.github.com>
2024-11-21 05:44:31 +0900

    [level 2] Title: 당구 연습, Time: 0.47 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/169198. 당구 연습/당구 연습.java
+++ 프로그래머스/2/169198. 당구 연습/당구 연습.java
@@ -1,63 +1,36 @@
-import java.util.*;
-
 class Solution {
-    
-    private static class Ball {
-        private int x, y;
-        
-        public Ball(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-    
-    private static Ball origin;
     public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
         int[] answer = new int[balls.length];
-        origin = new Ball(startX,startY);
-        
         for (int i=0;i<balls.length;i++) {
-            answer[i] = getMinDis(m,n,new Ball(balls[i][0],balls[i][1]));
+            
+            int dist = Integer.MAX_VALUE;
+            
+            int x = balls[i][0];
+            int y = balls[i][1];
+            
+            // y = 0 대칭
+            if (startX != x || y >= startY) {
+                dist = Math.min((int)(Math.pow(startX - x, 2) + Math.pow(-y-startY, 2)), dist);
+            }
+           
+            // y = m 대칭
+            if (startX != x || y <= startY) {
+                dist = Math.min((int)(Math.pow(startX - x, 2) + Math.pow(2*n-y-startY, 2)), dist);
+            }
+            
+            // x = 0 대칭
+            if (startY != y || x >= startX) {
+                dist = Math.min((int)(Math.pow(-x - startX, 2) + Math.pow(y-startY, 2)), dist);
+            }
+            
+            // x = m 대칭
+            if (startY != y || x <= startX) {
+                dist = Math.min((int)(Math.pow(2*m-x - startX, 2) + Math.pow(y-startY, 2)), dist);
+            }
+
+            answer[i] = dist;
         }
         
         return answer;
-    }
-    
-    
-    public static int getMinDis(int m, int n, Ball target){
-        int min = Integer.MAX_VALUE; 
-        
-        List<Ball> candidate = new ArrayList<>();
-        
-        // 위쪽 이동 (y=n 대칭이동)
-        if (!(origin.x == target.x && origin.y < target.y)) {
-            candidate.add(new Ball(target.x, 2*n-target.y));
-        }
-        
-        // 아래쪽 이동 (y=0 대칭이동)
-        if (!(origin.x == target.x && origin.y > target.y)) {
-             candidate.add(new Ball(target.x, -target.y));
-        }
-        
-        // 왼쪽 이동 (x=0 대칭이동)
-        if (!(origin.y == target.y && origin.x > target.x)) {
-             candidate.add(new Ball(-target.x, target.y));
-        }
-        
-        
-        // 오른쪽 이동 (x=m 대칭이동)
-        if (!(origin.y == target.y && origin.x < target.x)) {
-             candidate.add(new Ball(2*m - target.x, target.y));
-        }
-        
-        for (Ball now : candidate) {
-            int xAbs = Math.abs(origin.x - now.x);
-            int yAbs = Math.abs(origin.y - now.y);
-            
-            min = Math.min(min, (int)(Math.pow(xAbs,2) + Math.pow(yAbs,2)));
-        }
-        
-        
-        return min;
     }
 }

cb9a04d303358f1884771c5f06351c21270113c6
유송연 <86238720+syy0O@users.noreply.github.com>
2024-11-28 10:21:03 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 119.74 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -1,4 +1,4 @@
-// 브루트포스
+// 백트래킹
 class Solution {
     
     
@@ -30,7 +30,7 @@ class Solution {
     
     public void ticktakto(int cnt) {
        
-        if(isWinnerDecided() || cnt == 9) {
+        if(isWinnerDecided() || cnt == 9) { // 장기말을 다 두었거나, 승자가 정해졌을 때
            if (isSame()) {
                flag = true;
            }         
@@ -39,56 +39,41 @@ class Solution {
         
         for (int i=0;i<3;i++) {
             for (int j=0;j<3;j++) {
-
-                if (isSame()) {
+                
+                if (isSame()) { // origin 배열과, 현재 배열이 같아질 때
                     flag = true;
                     return;
                 }
 
                 
-                if (map[i][j] != '.') {
+                if (map[i][j] != '.') { // 이미 말이 놓아져 있을 때
                     continue;
                 }
                 
-                if (cnt % 2 == 0) {
-                    map[i][j] = 'O';
-                    ticktakto(cnt+1);
-                    map[i][j] = '.';
-                }
-                else {
-                    map[i][j] = 'X';
-                    ticktakto(cnt+1);
-                    map[i][j] = '.';
-                }
+                map[i][j] = cnt % 2 == 0 ? 'O' : 'X';
+                ticktakto(cnt+1);
+                map[i][j] = '.';
             }
         }
         
     }
     
-    public void print(Character[][] obj) {
-        for (int i=0;i<3;i++) {
-            for (int j=0;j<3;j++) {
-                System.out.print(obj[i][j]);
-            }
-            System.out.println();
-        }
-    }
     
     public boolean isWinnerDecided(){
         for (int i=0;i<3;i++) {
-            if (map[i][0]!='.' && map[i][0] == map[i][1] && map[i][1] == map[i][2]) {
+            if (map[i][0]!='.' && map[i][0] == map[i][1] && map[i][1] == map[i][2]) { // 가로
                 return true;
             }
-            if (map[0][i]!='.' && map[0][i] == map[1][i] && map[1][i] == map[2][i]) {
+            if (map[0][i]!='.' && map[0][i] == map[1][i] && map[1][i] == map[2][i]) { // 세로
                 return true;
             }
         }
         
-        if (map[0][0] != '.' && map[0][0] == map[1][1] && map[1][1] == map[2][2]) {
+        if (map[0][0] != '.' && map[0][0] == map[1][1] && map[1][1] == map[2][2]) { // 우하향 대각선
             return true;
         }
         
-         if (map[0][2] != '.' && map[0][2] == map[1][1] && map[1][1] == map[2][0]) {
+         if (map[0][2] != '.' && map[0][2] == map[1][1] && map[1][1] == map[2][0]) { // 우상향 대각선
             return true;
         }
         

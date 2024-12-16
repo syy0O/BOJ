40d4500fa89312eaff09358889eea86034b637f6
유송연 <86238720+syy0O@users.noreply.github.com>
2024-12-16 13:48:00 +0900

    [level 2] Title: 디펜스 게임, Time: 99.90 ms, Memory: 150 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    // O(N), O(NlogN), O(logN)
+    // 그리디.
+    // 우선순위큐(enemy 내림차 정렬)
+    public int solution(int n, int k, int[] enemy) {
+        int answer = 0;
+        
+        int sum = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        int i;
+        for (i=0;i<enemy.length;i++) {
+            sum += enemy[i];
+            pq.add(enemy[i]);
+            
+            if (sum <= n) {
+                continue;
+            }
+            
+            if (k == 0) {
+                break;
+            }
+            
+            int max = pq.poll();
+            sum -= max;
+            k--;
+        }
+        
+        answer = i;
+        
+        return answer;
+    }
+}

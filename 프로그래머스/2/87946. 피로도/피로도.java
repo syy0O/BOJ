80c620a23378b531920f96d7c9a1323d938937e4
유송연 <86238720+syy0O@users.noreply.github.com>
2025-01-23 06:21:43 +0900

    [level 2] Title: 피로도, Time: 2.73 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -16,7 +16,8 @@ class Solution {
     }
     
     
-    public static void findMaxDungeonCnt (int fatigue, int cnt) { 
+    public static void findMaxDungeonCnt (int fatigue, int cnt) { // 종료조건이 보이지 않는다면, 참일 때만 카운트 하게 하고, 나가게 되는 마지막에 정답을 업데이트 해주면 된다. 
+        // 백트래킹 더 연습해야할 듯 -> 다시 공부하기
         
         for (int i=0;i<copy.length;i++) {
             if (!visited[i] && fatigue >= copy[i][0]) {   
@@ -24,8 +25,6 @@ class Solution {
                 findMaxDungeonCnt(fatigue-copy[i][1], cnt+1);
                 visited[i] = false;
             }
-            
-            //answer = Math.max(answer, cnt);
         }
         
         answer = Math.max(answer, cnt);

dec46065a620bf231558b535b1a5c9d23e2a01c0
유송연 <86238720+syy0O@users.noreply.github.com>
2025-03-12 04:42:29 +0900

    [level 3] Title: 징검다리 건너기, Time: 60.87 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,42 +1,27 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] stones, int k) {
-        int answer = 0;
+        int answer = Integer.MAX_VALUE;
+        Deque<Integer> deque = new ArrayDeque<>();
         
-        int left = 1;
-        int right = 200000000;
-        
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (isPossible(stones, k, mid)) {
-                left = mid + 1;
-                answer = Math.max(answer,mid);
-            }
-            else {
-                right = mid-1;   
-            }
-        }
-    
-        return answer;
-    }
-    
-    
-    public boolean isPossible(int[] stones, int k , int target) {
-        boolean flag = true;
-        
-        int skip = 0;
-        for (int stone : stones) {
-            if (stone < target) {
-                skip++;
-            }
-            else {
-                skip = 0;
+        for (int i=0;i<stones.length;i++) {
+            
+            if (!deque.isEmpty() && deque.peekFirst() < i-k+1) { //슬라이딩 윈도우 범위를 벗어난 숫자
+                deque.pollFirst();
             }
             
-            if (skip == k) {
-                return false;
+            while (!deque.isEmpty() && stones[deque.peekLast()] < stones[i]) { // 앞으로의 모든 구간에서 답이 될수 없는 수는 남기지 않음
+                deque.pollLast();
+            }
+            
+            deque.addLast(i);
+            
+            if (i-k+1 >= 0) {
+                answer = Math.min(answer, stones[deque.peekFirst()]);
             }
         }
         
-        return flag;
+        return answer;
     }
 }
